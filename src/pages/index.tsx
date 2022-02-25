32535045c1654ad6ccb9e3b9121ac23ec3deff15
Rassul <rasul.tabulov@gmail.com>
2022-02-25 11:18:18 +0600

    add tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-3xl">hi</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
